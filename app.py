5fa5383018df1e2e87cbd3e4ffe4c8f564640c44
vicsim181 <victor.simon@outlook.com>
2022-08-08 20:34:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+from unicodedata import name
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+# To escape if HTML is returned by a fonction:
+    # from markupsafe import escape
+
+    # @app.route("/<name>")
+    # def hello(name):
+    #     return f"Hello, {escape(name)}!"
+
+@app.route('/')
+def projects():
+    return 'The HOME PAGE!'
