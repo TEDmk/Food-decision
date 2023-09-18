d191ae6e52187080bd3534f9c094c17f97ef7316
Victor <victor.simon@outlook.com>
2023-09-18 20:12:36 +0200

    modifications next -> add sqlalchemy

--- food_decision/blog.py
+++ food_decision/blog.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -13,11 +15,10 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, name, recipe, meal, types, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    print('\n POSTS \n', posts, '\n')
     return render_template('blog/index.html', posts=posts)
 
 
@@ -25,21 +26,23 @@ def index():
 @login_required
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        name = request.form['name']
+        recipe = request.form['recipe']
+        meal = json.dumps({'meal': request.form.getlist('meal')})
+        types = json.dumps({'types': request.form.getlist('types')})
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not name:
+            error = 'A name is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO post (name, recipe, meal, types, author_id)'
+                ' VALUES (?, ?, ?, ?, ?)',
+                (name, recipe, meal, types, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -49,7 +52,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, name, recipe, meal, types, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -60,7 +63,7 @@ def get_post(id, check_author=True):
 
     if check_author and post['author_id'] != g.user['id']:
         abort(403)
-
+    print('\n LALALAL ', post, '\n')
     return post
 
 
@@ -70,21 +73,21 @@ def update(id):
     post = get_post(id)
 
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        name = request.form['name']
+        recipe = request.form['recipe']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not name:
+            error = 'Name is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE post SET name = ?, recipe = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (name, recipe, id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
--- food_decision/forms/form.py
+++
@@ -1,5 +0,0 @@
-from wtforms import Form, StringField, validators
-
-
-class TestForm(Form):
-    test_field = StringField(label='Essai', validators=[validators.length(min=2, max=25)])
